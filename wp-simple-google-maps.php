a872935582b68c676dd00010a339086e1b0c72c5
Rio Bahtiar <akhibahtiar@gmail.com>
2021-04-26 20:23:34 +0700

    Bump. First commit

---
+++ wp-simple-google-maps.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: WP Simple Google Maps
+ * Plugin URI: https://rio.my.id
+ * Description: Just a simple plugin to show Google Maps
+ * Author: Rio Bahtiar
+ * Author URI: https://rio.my.id
+ * Text Domain: wp-simple-google-maps
+ * Domain Path: /languages
+ * Version: 0.0.1
+ *
+ * @package WPSGM
+ */
+
+defined( 'ABSPATH' ) || exit;
